dc24259a2b8854bb964f3a69cb0219e5514e8076
ajc7186 <82669695+ajc7186@users.noreply.github.com>
2023-11-08 15:53:12 -0500

    Update CheckInScan.php

--- CheckInScan.php
+++ CheckInScan.php
@@ -1,51 +1,65 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Check-In Item Scan</title>
+    <title>Check-In Item</title>
     <style>
         body {
             font-family: Arial, sans-serif;
             background-color: #f0f0f0;
             margin: 0;
             padding: 0;
+            text-align: center;
         }
 
         h2 {
             background-color: #007BFF;
             color: #fff;
-            padding: 10px;
+            padding: 15px;
         }
 
-        table {
-            width: 100%;
-            border-collapse: collapse;
+        form {
+            background-color: #fff;
+            border-radius: 10px;
+            padding: 20px;
+            width: 300px;
+            margin: 0 auto;
         }
 
-        table, th, td {
-            border: 1px solid #ccc;
-        }
-
-        th, td {
-            padding: 10px;
+        label {
+            display: block;
             text-align: left;
+            margin-top: 10px;
         }
 
-        th {
+        input {
+            width: 100%;
+            padding: 10px;
+            margin: 5px 0;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+        }
+
+        input[type="submit"] {
             background-color: #007BFF;
             color: #fff;
+            border: none;
+            border-radius: 5px;
+            padding: 10px;
+            cursor: pointer;
         }
 
-        tr:nth-child(even) {
-            background-color: #f2f2f2;
+        input[type="submit"]:hover {
+            background-color: #0056b3;
         }
 
         a {
-            display: block;
-            text-align: center;
+            text-decoration: none;
             background-color: #007BFF;
             color: #fff;
             padding: 10px;
-            text-decoration: none;
+            border-radius: 5px;
+            display: inline-block;
+            margin-top: 10px;
         }
 
         a:hover {
@@ -54,39 +68,40 @@
     </style>
 </head>
 <body>
-    <h2>Check-In Item Scan</h2>
+    <h2>Check-In Item</h2>
 
     <?php
-    // Include the database configuration
-    include "db_config.php";
-
-    // Scan item
-    $itemID = $_POST["itemID"];
-
-    // SQL query to check-in scanned item to update the "Item" table
-    $sql = "UPDATE Item 
-            SET itemStatus = 'Checked-In'
-            WHERE itemID = $itemID"
-
-    if ($conn->query($sql) === TRUE) {
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Include the database configuration
+        include "db_config.php";
+    
+        // Scan item
+        $itemID = $_POST["itemID"];
+    
+        // SQL query to check-in scanned item to update the "Item" table
+        $sql = "UPDATE Item 
+                SET itemStatus = 'Checked-In'
+                WHERE itemID = $itemID"
+    
+        if ($conn->query($sql) === TRUE) {
             echo "<p>Item successfully checked-in.</p>";
         } else {
             echo "<p>Error: " . $sql . "<br>" . $conn->error . "</p>";
         }
-      
-    // Close the database connection
+          
+        // Close the database connection
         $conn->close();
     }
     ?>
-
+    
     <form method="post" action="">
-
+    
         <label for="itemID">Item ID:</label>
         <input type="number" step="1" name="itemID" required max="65535">
-
+    
         <input type="submit" value="Check-in Item">
     </form>
-
+    
     <a href="welcome.php">Back to Welcome</a>
 </body>
 </html>
