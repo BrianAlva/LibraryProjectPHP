40c62672cf2b9ccc5ee12e07fcf395658aa9cff4
BrianAlva <briang.alvarez2@gmail.com>
2023-11-19 22:17:09 -0500

    Revert "Revert "Revert "Merge pull request #3 from BrianAlva/ProofofConcept-harshita"""
    This reverts commit 1b24521b6059ca1abe6c95143611b8991c56c2a7.

--- ManagePatrons.php
+++ ManagePatrons.php
@@ -1,95 +1,46 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-<title>Manage Patrons</title>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inconsolata">
-<style>
-body, html {
-  height: 100%;
-  font-family: "Inconsolata", sans-serif;
-}
+    <title>Welcome Page</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+        }
 
-.bgimg {
-  background-position: center;
-  background-size: cover;
-  background-image: url("managepatronbgimg.png");
-  min-height: 75%;
-}
+        h2 {
+            color: #333;
+        }
 
-.menu {
-  display: none;
-}
+        p {
+            color: #666;
+        }
 
-.info-box {
-  background-color: black;
-  padding: 20px;
-  text-align: center;
-  color: white;
-  margin-top: 20px;
-}
+        a {
+            text-decoration: none;
+            background-color: #007BFF;
+            color: #fff;
+            padding: 10px 20px;
+            margin: 5px 0;
+            border-radius: 5px;
+            display: block;
+        }
 
-.info-box button {
-  margin: 10px;
-}
-</style>
+        a:hover {
+            background-color: #0056b3;
+        }
+    </style>
 </head>
 <body>
+    <h2>Welcome to Library Directory</h2>
+    <p>You have successfully logged in.</p>
 
-<!-- Links (sit on top) -->
-<div class="w3-top">
-  <div class="w3-row w3-padding w3-black">
-    <div class="w3-col s3" style="width: 20%"><a href="welcome.php" class="w3-button w3-block w3-black">HOME</a></div>
-    <div class="w3-col s3" style="width: 20%"><a href="ManagePatrons.php" class="w3-button w3-block w3-black">MANAGE PATRONS</a></div>
-    <div class="w3-col s3" style="width: 20%"><a href="ManageItems" class="w3-button w3-block w3-black">MANAGE ITEMS</a></div>
-    <div class="w3-col s3" style="width: 20%"><a href="CheckOutItems.php" class="w3-button w3-block w3-black">CHECK OUT</a></div>
-    <div class="w3-col s3" style="width: 20%"><a href="#check-in" class="w3-button w3-block w3-black">CHECK IN</a></div>
-  </div>
-</div>
+    <!-- Buttons to navigate to different pages -->
+    <a href="CreatePatron.php">Create New Patron</a>
+    <a href="EditPatron.php">Edit Patron</a>
+    <a href="DeletePatron.php">Delete Patron</a>
 
-<!-- Header with image -->
-<header class="bgimg w3-display-container w3-grayscale-min" id="home">
-  <div class="w3-display-bottomleft w3-center w3-padding-large w3-hide-small">
-    <span class="w3-tag">Open from 8am to 10pm</span>
-  </div>
-  <div class="w3-display-top w3-center">
-    <span class="w3-text-white" style="font-size:55px"><br>Manage Patrons</span>
-    <div class="info-box">
-      <p>Please choose one of the following</p>
-      <a class="w3-button w3-black" href="CreatePatron.php">Create New Patron</a>
-      <button class="w3-button w3-black" onclick="editPatron()">Edit Patron</button>
-      <a class="w3-button w3-black" href="DeletePatron.php">Delete Patron</a>
-    </div>
-  </div>
-  <div class="w3-display-bottomright w3-center w3-padding-large w3-hide-small">
-    <span class="w3-tag">456 Literary Lane, 28403</span>
-  </div>
-</header>
+    <br><br>
 
-<!-- Add a background color and large text to the whole page -->
-<div class="w3-sand w3-grayscale w3-large">
-
-<!-- Footer -->
-<footer class="w3-center w3-light-grey w3-padding-48 w3-large">
-  <p>Powered by <a title="Wisdom" target="_blank" class="w3-hover-text-green">Wisdom</a></p>
-</footer>
-
-<script>
-// Functions
-function createNewPatron() {
-  alert("Create New Patron executed!");
-}
-
-function editPatron() {
-  alert("Edit Patron executed!");
-}
-
-function deletePatron() {
-  alert("Delete Patron executed!");
-}
-</script>
-
+    <a href="welcome.php">Back to Home</a> <!-- Provide a link to log out -->
 </body>
 </html>
