a114f8d1b88e00a322a923833e3a198aee996fb0
cusagar <129072932+cusagar@users.noreply.github.com>
2023-11-26 17:58:16 -0500

    Update ManageItems.php

--- ManageItems.php
+++ ManageItems.php
@@ -10,13 +10,15 @@
 body, html {
   height: 100%;
   font-family: "Inconsolata", sans-serif;
+  margin: 0;
+  padding: 0;
 }
 
 .bgimg {
   background-position: center;
   background-size: cover;
-  background-image: url("https://images.unsplash.com/photo-1595123550441-d377e017de6a?q=80&w=3106&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
-      min-height: 75%;
+  background-image: url("https://images.unsplash.com/photo-1572061486195-d811e12d0a10?q=80&w=2942&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
+  min-height: 75%;
 }
 
 .menu {
@@ -34,19 +36,59 @@ body, html {
 .info-box button {
   margin: 10px;
 }
+
+.login-box {
+  background-color: black;
+  width: 300px;
+  padding: 20px;
+  text-align: center;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  border-radius: 10px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+}
+
+.login-box input {
+  width: 100%;
+  padding: 10px;
+  margin-bottom: 15px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+}
+
+.login-box button {
+  width: 100%;
+  padding: 10px;
+  background-color: #4CAF50;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+.login-box button:hover {
+  background-color: #45a049;
+}
+
+.w3-display-top {
+  margin-top: 50px; /* Adjust the margin to move the text lower */
+}
 </style>
 </head>
 <body>
 
-<!-- Links (sit on top) -->
+    <!-- Links (sit on top) -->
 <div class="w3-top">
-  <div class="w3-row w3-padding w3-black">
-  <div class="w3-col s3" style="width: 20%"><a href="welcome.php" class="w3-button w3-block w3-black">HOME</a></div>
-    <div class="w3-col s3" style="width: 20%"><a href="ManagePatrons.php" class="w3-button w3-block w3-black">MANAGE PATRONS</a></div>
-    <div class="w3-col s3" style="width: 20%"><a href="ManageItems.php" class="w3-button w3-block w3-black">MANAGE ITEMS</a></div>
-    <div class="w3-col s3" style="width: 20%"><a href="NewCheckOut.php" class="w3-button w3-block w3-black">CHECK OUT</a></div>
-    <div class="w3-col s3" style="width: 20%"><a href="CheckInScan.php" class="w3-button w3-block w3-black">CHECK IN</a></div>
-  </div>
+    <div class="w3-row w3-padding w3-black">
+        <div class="w3-col s2" style="width: 10%"><a href="welcome.php" class="w3-button w3-block w3-black" title="Go to Home">HOME</a></div>
+        <div class="w3-col s2" style="width: 20%"><a href="ManagePatrons.php" class="w3-button w3-block w3-black" title="Create new Patron&#10;Edit Patron&#10;Delete Patron">MANAGE PATRONS</a></div>
+        <div class="w3-col s2" style="width: 18%"><a href="ManageItems.php" class="w3-button w3-block w3-black" title="View all items&#10;Create new items&#10;Delete items&#10;Edit items">MANAGE ITEMS</a></div>
+        <div class="w3-col s2" style="width: 15%"><a href="NewCheckOut.php" class="w3-button w3-block w3-black" title="Check out items">CHECK OUT</a></div>
+        <div class="w3-col s2" style="width: 15%"><a href="CheckInScan.php" class="w3-button w3-block w3-black" title="Check in items">CHECK IN</a></div>
+        <div class="w3-col s2" style="width: 20%"><a href="Reshelving.php" class="w3-button w3-block w3-black" title="Manage reshelving of items">RESHELVE ITEMS</a></div>
+    </div>
 </div>
 
 <!-- Header with image -->
@@ -55,7 +97,7 @@ body, html {
     <span class="w3-tag">Open from 8am to 10pm</span>
   </div>
   <div class="w3-display-top w3-center">
-    <span class="w3-text-white" style="font-size:55px"><br>Manage Items</span>
+    <span class="w3-text-white" style="font-size:37px"><br>Manage Items</span>
     <div class="info-box">
       <p>Please choose one of the following</p>
       <button class="w3-button w3-black" onclick="viewAllItems()">View All Items</button>
@@ -63,7 +105,8 @@ body, html {
       <button class="w3-button w3-black" onclick="deleteItem()">Delete Item</button>
       <button class="w3-button w3-black" onclick="editItem()">Edit Item</button>
     </div>
-  </div>
+  
+  
   <div class="w3-display-bottomright w3-center w3-padding-large w3-hide-small">
     <span class="w3-tag">456 Literary Lane, 28403</span>
   </div>
@@ -76,7 +119,6 @@ body, html {
 <footer class="w3-center w3-light-grey w3-padding-48 w3-large">
   <p>Powered by <a title="Wisdom" target="_blank" class="w3-hover-text-green">Wisdom</a></p>
 </footer>
-
 <script>
 // Functions
 function viewAllItems() {
@@ -102,4 +144,3 @@ function editItem() {
 
 </body>
 </html>
-
