17cf89d1daa2aa12f32a96ca11a5f7abd61b46e6
cusagar <129072932+cusagar@users.noreply.github.com>
2023-11-26 17:58:44 -0500

    Update CheckInScan.php

--- CheckInScan.php
+++ CheckInScan.php
@@ -10,43 +10,73 @@
 body, html {
   height: 100%;
   font-family: "Inconsolata", sans-serif;
+  margin: 0;
+  padding: 0;
 }
 
 .bgimg {
   background-position: center;
   background-size: cover;
-  background-image: url("https://images.unsplash.com/photo-1595123550441-d377e017de6a?q=80&w=3106&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
-      min-height: 75%;
+  background-image: url("https://images.unsplash.com/photo-1572061486195-d811e12d0a10?q=80&w=2942&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
+  min-height: 75%;
 }
 
 .menu {
   display: none;
 }
 
-.info-box {
+.login-box {
   background-color: black;
+  width: 300px;
   padding: 20px;
   text-align: center;
-  color: white;
-  margin-top: 20px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  border-radius: 10px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
 }
 
-.info-box button {
-  margin: 10px;
+.login-box input {
+  width: 100%;
+  padding: 10px;
+  margin-bottom: 15px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+}
+
+.login-box button {
+  width: 100%;
+  padding: 10px;
+  background-color: #4CAF50;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+.login-box button:hover {
+  background-color: #45a049;
+}
+
+.w3-display-top {
+  margin-top: 50px; /* Adjust the margin to move the text lower */
 }
 </style>
 </head>
 <body>
 
-<!-- Links (sit on top) -->
+    <!-- Links (sit on top) -->
 <div class="w3-top">
-  <div class="w3-row w3-padding w3-black">
-    <div class="w3-col s3" style="width: 20%"><a href="welcome.php" class="w3-button w3-block w3-black">HOME</a></div>
-    <div class="w3-col s3" style="width: 20%"><a href="ManagePatrons.php" class="w3-button w3-block w3-black">MANAGE PATRONS</a></div>
-    <div class="w3-col s3" style="width: 20%"><a href="ManageItems.php" class="w3-button w3-block w3-black">MANAGE ITEMS</a></div>
-    <div class="w3-col s3" style="width: 20%"><a href="NewCheckOut.php" class="w3-button w3-block w3-black">CHECK OUT</a></div>
-    <div class="w3-col s3" style="width: 20%"><a href="CheckInScan.php" class="w3-button w3-block w3-black">CHECK IN</a></div>
-  </div>
+    <div class="w3-row w3-padding w3-black">
+        <div class="w3-col s2" style="width: 10%"><a href="welcome.php" class="w3-button w3-block w3-black" title="Go to Home">HOME</a></div>
+        <div class="w3-col s2" style="width: 20%"><a href="ManagePatrons.php" class="w3-button w3-block w3-black" title="Create new Patron&#10;Edit Patron&#10;Delete Patron">MANAGE PATRONS</a></div>
+        <div class="w3-col s2" style="width: 18%"><a href="ManageItems.php" class="w3-button w3-block w3-black" title="View all items&#10;Create new items&#10;Delete items&#10;Edit items">MANAGE ITEMS</a></div>
+        <div class="w3-col s2" style="width: 15%"><a href="NewCheckOut.php" class="w3-button w3-block w3-black" title="Check out items">CHECK OUT</a></div>
+        <div class="w3-col s2" style="width: 15%"><a href="CheckInScan.php" class="w3-button w3-block w3-black" title="Check in items">CHECK IN</a></div>
+        <div class="w3-col s2" style="width: 20%"><a href="Reshelving.php" class="w3-button w3-block w3-black" title="Manage reshelving of items">RESHELVE ITEMS</a></div>
+    </div>
 </div>
 
 <!-- Header with image -->
@@ -55,59 +85,61 @@ body, html {
     <span class="w3-tag">Open from 8am to 10pm</span>
   </div>
   <div class="w3-display-top w3-center">
-    <span class="w3-text-white" style="font-size:55px"><br>Check In Item</span>
-    <?php
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // Include the database configuration
-        include "db_config.php";
+    <span class="w3-text-white" style="font-size:37px"><br>Check In Item</span>
+  </div>
+  <?php
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+      // Include the database configuration
+      include "db_config.php";
 
-        // Scan item
-        $itemID = $_POST["itemID"];
+      // Scan item
+      $itemID = $_POST["itemID"];
 
-        // Check if ItemID exists and is checked-out
-        $checkItemQuery = "SELECT *
-                          FROM Item
-                          WHERE itemID = '$itemID'
-                          AND itemStatus = 'Checked Out'";
-        // AND itemStatus = 'Checked Out'";
-        $itemResult = $conn->query($checkItemQuery);
+      // Check if ItemID exists and is checked-out
+      $checkItemQuery = "SELECT *
+                        FROM Item
+                        WHERE itemID = '$itemID'
+                        AND itemStatus = 'Checked Out'";
+      // AND itemStatus = 'Checked Out'";
+      $itemResult = $conn->query($checkItemQuery);
 
-        if ($itemResult->num_rows > 0) {
+      if ($itemResult->num_rows > 0) {
 
-            // SQL query to check-in scanned item to update the "Item" table
-            $sql = "UPDATE Item
-                    SET itemStatus = 'Checked In'
-                    WHERE itemID = '$itemID'";
+          // SQL query to check-in scanned item to update the "Item" table
+          $sql = "UPDATE Item
+                  SET itemStatus = 'Checked In'
+                  WHERE itemID = '$itemID'";
 
-            $sql2 = "UPDATE checkoutTransactionItem
-                    SET TransactionItemStatus = 'Checked In'
-                    WHERE itemID = '$itemID'";
+          $sql2 = "UPDATE checkoutTransactionItem
+                  SET TransactionItemStatus = 'Checked In'
+                  WHERE itemID = '$itemID'";
 
-            if ($conn->query($sql) === TRUE && $conn->query($sql2) === TRUE) {
-                echo "<p>Item successfully checked-in.</p>";
-            } else {
-                echo "<p>Error: " . $sql . "<br>" . $conn->error . "</p>";
-            }
+          if ($conn->query($sql) === TRUE && $conn->query($sql2) === TRUE) {
+              echo "<p>Item successfully checked-in.</p>";
+          } else {
+              echo "<p>Error: " . $sql . "<br>" . $conn->error . "</p>";
+          }
 
-        }
+      }
 
-        else {
-            echo "<p>Error: Item ID is not available or is available.</p>";
-            }
+      else {
+          echo "<p>Error: Item ID is not available or is available.</p>";
+          }
 
-        // Close the database connection
-        $conn->close();
-    }
-    ?>
+      // Close the database connection
+      $conn->close();
+  }
+  ?>
+  <form method="post" action=""></form>
+    <div class="login-box">
+      
 
-    <form method="post" action="">
+      <label for="itemID" >Item ID:</label>
+      <input type="number" step="1" name="itemID" placeholder="Item ID:" >
 
-        <label for="itemID" style="color: grey;">Item ID:</label>
-        <input type="number" step="1" name="itemID" required max="65535">
-
-        <input type="submit" value="Check-in Item">
-    </form>
-    <a href="index.html">Back to Welcome</a>
+      <button type="submit">Check-in Item</button>
+    </div>
+  </form>
   <div class="w3-display-bottomright w3-center w3-padding-large w3-hide-small">
     <span class="w3-tag">456 Literary Lane, 28403</span>
   </div>
@@ -124,4 +156,3 @@ body, html {
 
 </body>
 </html>
-
