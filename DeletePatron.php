aa93dbce5269f73ed6bd2e58d532551576a767cd
Harshipanda0809 <150968316+Harshipanda0809@users.noreply.github.com>
2023-11-27 12:00:59 -0500

    Update DeletePatron.php

--- DeletePatron.php
+++ DeletePatron.php
@@ -1,93 +1,85 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Delete Patron</title>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inconsolata">
-  <style>
-    body, html {
-      height: 100%;
-      font-family: "Inconsolata", sans-serif;
-      margin: 0;
-      padding: 0;
-    }
+<title>W3.CSS Template</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inconsolata">
+<style>
+body, html {
+  height: 100%;
+  font-family: "Inconsolata", sans-serif;
+  margin: 0;
+  padding: 0;
+}
 
-    .bgimg {
-      background-position: center;
-      background-size: cover;
-      background-image: url("https://images.unsplash.com/photo-1595123550441-d377e017de6a?q=80&w=3106&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
-    }
+.bgimg {
+  background-position: center;
+  background-size: cover;
+  background-image: url("https://images.unsplash.com/photo-1572061486195-d811e12d0a10?q=80&w=2942&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
+  min-height: 75%;
+}
 
-    .menu {
-      display: none;
-    }
+.menu {
+  display: none;
+}
+.white-text {
+    color: white;
+}
 
-    .login-box {
-      background-color: black;
-      width: 300px;
-      padding: 20px;
-      text-align: center;
-      position: absolute;
-      top: 50%;
-      left: 50%;
-      transform: translate(-50%, -50%);
-      border-radius: 10px;
-      box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-    }
+.login-box {
+  background-color: black;
+  width: 300px;
+  padding: 20px;
+  text-align: center;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  border-radius: 10px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+}
 
-    .login-box input {
-      width: 100%;
-      padding: 10px;
-      margin-bottom: 15px;
-      border: 1px solid #ccc;
-      border-radius: 5px;
-    }
+.login-box input {
+  width: 100%;
+  padding: 10px;
+  margin-bottom: 15px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+}
 
-    .login-box button {
-      width: 100%;
-      padding: 10px;
-      background-color: #4CAF50;
-      color: white;
-      border: none;
-      border-radius: 5px;
-      cursor: pointer;
-    }
+.login-box button {
+  width: 100%;
+  padding: 10px;
+  background-color: #4CAF50;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
 
-    .login-box button:hover {
-      background-color: #45a049;
-    }
+.login-box button:hover {
+  background-color: #45a049;
+}
 
-    .w3-display-top {
-      /*margin-top: 50px;  Adjust the margin to move the text lower */
-	  
-    }
-	
-	.w3-black {
-		height: 60px;
-	}
-	
-	.delForm {
-			margin-top : 70px;
-			
-			margin-left : 150px;
-			
-	}
-  </style>
+.w3-display-top {
+  margin-top: 50px; /* Adjust the margin to move the text lower */
+}
+</style>
 </head>
 <body>
 
-<!-- Links (sit on top) -->
+    <!-- Links (sit on top) -->
 <div class="w3-top">
     <div class="w3-row w3-padding w3-black">
-	<div class="w3-col s3" style="width: 100%;font-size:37px" class="w3-text-white">Delete Patron Record</div>
-    <div class="w3-col s3" style="width: 20%"><a href="welcome.php" class="w3-button w3-block w3-black">HOME</a></div>
-    <div class="w3-col s3" style="width: 20%"><a href="ManagePatrons.php" class="w3-button w3-block w3-black">MANAGE PATRONS</a></div>
-    <div class="w3-col s3" style="width: 20%"><a href="ManageItems.php" class="w3-button w3-block w3-black">MANAGE ITEMS</a></div>
-    <div class="w3-col s3" style="width: 20%"><a href="NewCheckOut.php" class="w3-button w3-block w3-black">CHECK OUT</a></div>
-    <div class="w3-col s3" style="width: 20%"><a href="CheckInScan.php" class="w3-button w3-block w3-black">CHECK IN</a></div>
-  </div>
+        <div class="w3-col s2" style="width: 10%"><a href="welcome.php" class="w3-button w3-block w3-black" title="Go to Home">HOME</a></div>
+        <div class="w3-col s2" style="width: 20%"><a href="ManagePatrons.php" class="w3-button w3-block w3-black" title="Create new Patron&#10;Edit Patron&#10;Delete Patron">MANAGE PATRONS</a></div>
+        <div class="w3-col s2" style="width: 18%"><a href="ManageItems.php" class="w3-button w3-block w3-black" title="View all items&#10;Create new items&#10;Delete items&#10;Edit items">MANAGE ITEMS</a></div>
+        <div class="w3-col s2" style="width: 15%"><a href="NewCheckOut.php" class="w3-button w3-block w3-black" title="Check out items">CHECK OUT</a></div>
+        <div class="w3-col s2" style="width: 15%"><a href="CheckInScan.php" class="w3-button w3-block w3-black" title="Check in items">CHECK IN</a></div>
+        <div class="w3-col s2" style="width: 20%"><a href="Reshelving.php" class="w3-button w3-block w3-black" title="Manage reshelving of items">RESHELVE ITEMS</a></div>
+    </div>
 </div>
 
 <!-- Header with image -->
@@ -96,8 +88,9 @@
     <span class="w3-tag">Open from 8am to 10pm</span>
   </div>
   <div class="w3-display-top w3-center">
-    <span class="w3-text-white" style="font-size:37px"><br></span>
-	<?php
+    <span class="w3-text-white" style="font-size:37px"><br>Delete Patron</span>
+  </div>
+  <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Include the database configuration
         include "db_config.php";
@@ -122,28 +115,19 @@
         $conn->close();
     }
     ?>
-  </div>
-  <!--<div class="login-box">
-    <label for="username">Username</label>
-    <input type="text" id="username" name="username" placeholder="Patron ID:">
-    <button onclick="login()">Delete Patron Record</button>
-	  
-	
-  </div> -->
-  
-  <div class="delForm">
-  <form method="post" action="">
-        <label for="patronID">Patron ID:</label>
-        <input type="number" name="patronID" required>
 
-        <input type="submit" value="Delete Patron Record">
-    </form>
-	
-	<div style="width: 20%;padding-left: 70px;padding-top: 30px;font-size:26px">
-		<a href="welcome.php" class="w3-button w3-block w3-black">Back to HOME</a>
-	</div>
-	
-  </div>	
+<form method="post" action=""></form>
+    <div class="login-box">
+      
+      <label for="patronID">Patron ID:</label>
+      <input type="number" name="patronID" required>
+
+        <button type="submit">Delete Patron Record</button>
+
+      
+    </div>
+  </form>
+
   <div class="w3-display-bottomright w3-center w3-padding-large w3-hide-small">
     <span class="w3-tag">456 Literary Lane, 28403</span>
   </div>
@@ -152,7 +136,12 @@
 <!-- Add a background color and large text to the whole page -->
 <div class="w3-sand w3-grayscale w3-large">
 
-  <!-- Footer -->
-  <footer class="w3-center w3-light-grey w3-padding-48 w3-large">
-    <p>Powered by <a title="Wisdom" target="_blank" class="w3-hover-text-green">Wisdom</a></p>
-  </footer>
+<!-- Footer -->
+<footer class="w3-center w3-light-grey w3-padding-48 w3-large">
+  <p>Powered by <a title="Wisdom" target="_blank" class="w3-hover-text-green">Wisdom</a></p>
+</footer>
+
+
+
+</body>
+</html>
