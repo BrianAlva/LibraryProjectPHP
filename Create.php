245fad991d31e69b620848a269a712ffcbb04e58
cusagar <129072932+cusagar@users.noreply.github.com>
2023-11-27 14:16:26 -0500

    Update Create.php

--- Create.php
+++ Create.php
@@ -1,76 +1,96 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <title>Create New Item</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            background-color: #f0f0f0;
-            margin: 0;
-            padding: 0;
-            text-align: center;
-        }
+<title>W3.CSS Template</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inconsolata">
+<style>
+body, html {
+  height: 100%;
+  font-family: "Inconsolata", sans-serif;
+  margin: 0;
+  padding: 0;
+}
 
-        h2 {
-            background-color: #007BFF;
-            color: #fff;
-            padding: 15px;
-        }
+.bgimg {
+  background-position: center;
+  background-size: cover;
+  background-image: url("https://images.unsplash.com/photo-1572061486195-d811e12d0a10?q=80&w=2942&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
+  min-height: 75%;
+}
 
-        form {
-            background-color: #fff;
-            border-radius: 10px;
-            padding: 20px;
-            width: 300px;
-            margin: 0 auto;
-        }
+.menu {
+  display: none;
+}
+.white-text {
+    color: white;
+}
 
-        label {
-            display: block;
-            text-align: left;
-            margin-top: 10px;
-        }
+.login-box {
+  background-color: black;
+  width: 200px;
+  padding: 0px;
+  text-align: center;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  border-radius: 1px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+}
 
-        input {
-            width: 100%;
-            padding: 10px;
-            margin: 5px 0;
-            border: 1px solid #ccc;
-            border-radius: 5px;
-        }
+.login-box input {
+  width: 100%;
+  padding: 1px;
+  margin-bottom: 1px;
+  border: 1px solid #ccc;
+  border-radius: 1px;
+}
 
-        input[type="submit"] {
-            background-color: #007BFF;
-            color: #fff;
-            border: none;
-            border-radius: 5px;
-            padding: 10px;
-            cursor: pointer;
-        }
+.login-box button {
+  width: 100%;
+  padding: 1px;
+  background-color: #4CAF50;
+  color: white;
+  border: none;
+  border-radius: 1px;
+  cursor: pointer;
+}
 
-        input[type="submit"]:hover {
-            background-color: #0056b3;
-        }
+.login-box button:hover {
+  background-color: #45a049;
+}
 
-        a {
-            text-decoration: none;
-            background-color: #007BFF;
-            color: #fff;
-            padding: 10px;
-            border-radius: 5px;
-            display: inline-block;
-            margin-top: 10px;
-        }
-
-        a:hover {
-            background-color: #0056b3;
-        }
-    </style>
+.w3-display-top {
+  margin-top: 50px; /* Adjust the margin to move the text lower */
+}
+</style>
 </head>
 <body>
-    <h2>Create New Item</h2>
 
-    <?php
+    <!-- Links (sit on top) -->
+<div class="w3-top">
+    <div class="w3-row w3-padding w3-black">
+        <div class="w3-col s2" style="width: 10%"><a href="welcome.php" class="w3-button w3-block w3-black" title="Go to Home">HOME</a></div>
+        <div class="w3-col s2" style="width: 20%"><a href="ManagePatrons.php" class="w3-button w3-block w3-black" title="Create new Patron&#10;Edit Patron&#10;Delete Patron">MANAGE PATRONS</a></div>
+        <div class="w3-col s2" style="width: 18%"><a href="ManageItems.php" class="w3-button w3-block w3-black" title="View all items&#10;Create new items&#10;Delete items&#10;Edit items">MANAGE ITEMS</a></div>
+        <div class="w3-col s2" style="width: 15%"><a href="NewCheckOut.php" class="w3-button w3-block w3-black" title="Check out items">CHECK OUT</a></div>
+        <div class="w3-col s2" style="width: 15%"><a href="CheckInScan.php" class="w3-button w3-block w3-black" title="Check in items">CHECK IN</a></div>
+        <div class="w3-col s2" style="width: 20%"><a href="Reshelving.php" class="w3-button w3-block w3-black" title="Manage reshelving of items">RESHELVE ITEMS</a></div>
+    </div>
+</div>
+
+<!-- Header with image -->
+<header class="bgimg w3-display-container w3-grayscale-min" id="home">
+  <div class="w3-display-bottomleft w3-center w3-padding-large w3-hide-small">
+    <span class="w3-tag">Open from 8am to 10pm</span>
+  </div>
+  <div class="w3-display-top w3-center">
+    <span class="w3-text-white" style="font-size:37px"><br>Create New Item</span>
+  </div>
+  <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Include the database configuration
         include "db_config.php";
@@ -103,15 +123,16 @@
     }
     ?>
 
-    <form method="post" action="">
-
-        <label for="itemISBN">ISBN:</label>
+<form method="post" action=""></form>
+    <div class="login-box">
+      
+    <label for="itemISBN" class="white-text">ISBN:</label>
         <input type="text" name="itemISBN" required maxlength="17">
 
-        <label for="itemTitle">Title:</label>
+        <label for="itemTitle" class="white-text">Title:</label>
         <input type="text" name="itemTitle" required maxlength="90">
 
-        <label for="itemType">Type:</label>
+        <label for="itemType" class="white-text">Type:</label>
         <select name="itemType" required>
             <option value="books">Books</option>
             <option value="periodicals">Periodicals</option>
@@ -119,19 +140,19 @@
             <option value="videos">Videos</option>
         </select>
 
-        <label for="itemYearPublished">Year Published:</label>
+        <label for="itemYearPublished" class="white-text">Year Published:</label>
         <input type="number" name="itemYearPublished" required max = "2023">
 
-        <label for="itemPublisher">Publisher:</label>
+        <label for="itemPublisher" class="white-text">Publisher:</label>
         <input type="text" name="itemPublisher" required maxlength="45">
 
-        <label for="itemLoC">Library of Congress:</label>
+        <label for="itemLoC" class="white-text">Library of Congress:</label>
         <input type="text" name="itemLoC" required maxlength="16">
 
-        <label for="itemCost">Cost:</label>
+        <label for="itemCost" class="white-text">Cost:</label>
         <input type="number" step="0.01" name="itemCost" required max="99999.99">
 
-        <label for="itemBranch">Branch:</label>
+        <label for="itemBranch" class="white-text">Branch:</label>
         <select name="itemBranch" required>
             <option value="Main">Main</option>
             <option value="OakTree">OakTree</option>
@@ -139,19 +160,19 @@
             <option value="WillowTree">WillowTree</option>
         </select>
 
-        <label for="itemStatus">Status:</label>
+        <label for="itemStatus" class="white-text">Status:</label>
         <select name="itemStatus" required>
             <option value="Available">Available</option>
             <option value="Not Available">Not Available</option>
         </select>
 
-        <label for="itemSecurityDeviceFlag">Security Device Flag:</label>
+        <label for="itemSecurityDeviceFlag" class="white-text">Security Device Flag:</label>
         <select name="itemSecurityDeviceFlag" required>
             <option value="Activated">Activated</option>
             <option value="Deactivated">Deactivated</option>
         </select>
 
-        <label for="itemDamage">Damage:</label>
+        <label for="itemDamage" class="white-text">Damage:</label>
         <select name="itemDamage" required>
             <option value="Like New">Like New</option>
             <option value="Great">Great</option>
@@ -159,10 +180,26 @@
             <option value="Unrepairable">Unrepairable</option>
         </select>
 
-        <input type="submit" value="Add New Item">
-    </form>
+        <button type="submit">Add New Item</button>
 
-    <a href="welcome.php">Back to Welcome</a>
+      
+    </div>
+  </form>
+
+  <div class="w3-display-bottomright w3-center w3-padding-large w3-hide-small">
+    <span class="w3-tag">456 Literary Lane, 28403</span>
+  </div>
+</header>
+
+<!-- Add a background color and large text to the whole page -->
+<div class="w3-sand w3-grayscale w3-large">
+
+<!-- Footer -->
+<footer class="w3-center w3-light-grey w3-padding-48 w3-large">
+  <p>Powered by <a title="Wisdom" target="_blank" class="w3-hover-text-green">Wisdom</a></p>
+</footer>
+
+
+
 </body>
 </html>
-
